e982598230f43f7b6e27c315e86db8bcaf0a3c9d
PixelBreeze <thepixelbreeze@gmail.com>
2015-07-28 02:42:07 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,10 +1,10 @@
 //Potato-Helper
 //Plug.dj Chat Utility Script. Fun and useful commands for chat.
 /*=====================================*/
-$('head').append("<style>" + "#chat-messages .id-6175571 .badge-box i.bdg { background-image: url('http://i.imgur.com/QtZP01G.png'); background-size:cover; }" + "</style>");
+$('head').append("<style>" + "#chat-messages .id-6175571 .badge-box i.bdg { background-image: url('http://i.imgur.com/JdcjpNI.png'); background-size:cover; }" + "</style>");
 $('#chat-messages').append('<div style="width:300px;height:55px;border-left:3px solid cyan;"</div><i style="width:33px;height:30px;margin-left:5px;" class="icon icon-site-logo"></i><div style="color:cyan;height:25px;padding-left:45px;padding-top:7px;">Welcome to Potato Helper! If you are a potato this utility is for you! List of commands - /cmds</div>');
 $('#chat-messages').append('<div style="width:300px;height:30px;border-left:3px solid cyan;"</div><div style="color:cyan;height:25px;padding-left:45px;padding-top:7px;">|PH| Credits: PixelBreezeNC, Zaro38</div>');
-var currentUsername = '@' + API.getUser().username; //the @name of the person who runs the script
+var currentUsername = '@' + API.getUser().username; //the @name of the person who runs the script http://i.imgur.com/JdcjpNI.png http://i.imgur.com/QtZP01G.png
 var afkReason = 'I am AFK right now!'; //standard afk reason
 var isAFK = false; //you are standard not afk
 var respondRCS = false; //responder for RCS

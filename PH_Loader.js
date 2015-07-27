a55a4ab02798b013ea0e4e525a4db60f1570db9c
PixelBreeze <thepixelbreeze@gmail.com>
2015-07-28 00:37:49 +0300

    Update PH_Loader.js

--- PH_Loader.js
+++ PH_Loader.js
@@ -4,4 +4,9 @@ this.data = data;
 if (data === '/loadph') {
 $.getScript('https://rawgit.com/PixelBreeze/Potato-Helper/master/Main.js');
   }
-}
+  if (data === '/reloadph') {
+      API.off(API.CHAT,GlobalCommands);
+      API.off(API.CHAT,PHchatCommands);
+      setTimeout(function(){$.getScript('https://rawgit.com/PixelBreeze/Potato-Helper/master/Main.js');},2000);
+    }
+  }

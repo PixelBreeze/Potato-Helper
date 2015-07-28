2d646bcc1992c338ecb400290fd02e0b941b4a69
PixelBreeze <thepixelbreeze@gmail.com>
2015-07-28 03:11:54 +0300

    Update PH_Loader.js

--- PH_Loader.js
+++ PH_Loader.js
@@ -6,6 +6,7 @@ this.data = data;
   if (data === '/reloadph') {
       API.off(API.CHAT,GlobalCommands);
       API.off(API.CHAT_COMMAND,PHchatCommands);
+      $('#chat-messages').append('<div style="width:300px;height:30px;border-left:3px solid orange;"</div><i style="width:33px;height:30px;margin-left:5px;" class="icon icon-history-white"></i><div style="color:orange;height:25px;padding-left:45px;padding-top:7px;">|PH| Reloading...</div>');
       API.chatLog("|PH| Reloading...");
       setTimeout(function(){$.getScript('https://rawgit.com/PixelBreeze/Potato-Helper/master/Main.js');},2000);
     }

7c9e2ac8a4caf6075859d27cc900dfd53210acfb
PixelBreeze <thepixelbreeze@gmail.com>
2015-08-01 01:11:23 +0300

    Update PH_Loader.js

--- PH_Loader.js
+++ PH_Loader.js
@@ -7,12 +7,12 @@ this.data = data;
       API.off(API.CHAT,GlobalCommands);
       API.off(API.CHAT_COMMAND,PHchatCommands);
       $('#chat-messages').append('<div style="width:300px;height:30px;border-left:3px solid orange;"</div><i style="width:33px;height:30px;margin-left:5px;" class="icon icon-history-white"></i><div style="color:orange;height:25px;padding-left:45px;padding-top:7px;">|PH| Reloading...</div>');
-      API.chatLog("|PH| Reloading...");
       setTimeout(function(){$.getScript('https://rawgit.com/PixelBreeze/Potato-Helper/master/Main.js');},2000);
     }
   if (data ==='/phoff') {
     API.off(API.CHAT,GlobalCommands);
     API.off(API.CHAT_COMMAND,PHchatCommands);
+    API.chatLog("PH shutting down.")
   }
   }
   //javascript:(function(){$.getScript('https://rawgit.com/PixelBreeze/Potato-Helper/master/PH_Loader.js');}());

5cca2e845dbb71092615c42eda013bd29d1f8a91
PixelBreeze <thepixelbreeze@gmail.com>
2015-08-30 23:51:57 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,7 +1,7 @@
 //Potato-Helper
 //Plug.dj Chat Utility Script. Fun and useful commands for chat.
 /*=====================================*/
-API.chatLog('PH version: 1.8');
+API.chatLog('PH version: 1.9');
 $('body').append('<style> [data-cid^="6175571"] .msg .from .un { color: rgba(250, 5, 54, 1) !important; } </style>');
 $('#chat-messages').append('<div style="width:300px;height:55px;border-left:3px solid cyan;"</div><i style="width:33px;height:30px;margin-left:5px;" class="icon icon-site-logo"></i><div style="color:cyan;height:25px;padding-left:45px;padding-top:7px;">Welcome to Potato Helper! If you are a potato this utility is for you! List of commands - /cmds</div>');
 $('#chat-messages').append('<div style="width:300px;height:30px;border-left:3px solid cyan;"</div><div style="color:cyan;height:25px;padding-left:45px;padding-top:7px;">|PH| Credits: PixelBreezeNC, Zaro38</div>');
@@ -163,7 +163,7 @@ function GlobalCommands(data) {
        		checkStream();
 		break;
        case '!setstream':
-			var currentChannel = data.slice(11, 355);
+			var currentChannel = message.slice(11, 355);
 		break;
        case 'skip':
            if (API.getUser(data.uid).role === 0) {

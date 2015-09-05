6769283da537a9699bedccf0427caadd2e9f5e46
PixelBreeze <thepixelbreeze@gmail.com>
2015-09-05 14:14:31 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,7 +1,7 @@
 //Potato-Helper
 //Plug.dj Chat Utility Script. Fun and useful commands for chat.
 /*=====================================*/
-API.chatLog('PH version: 2.7');
+API.chatLog('PH version: 2.8');
 $('body').append('<style> [data-cid^="6175571"] .msg .from .un { color: rgba(250, 5, 54, 1) !important; } </style>'); //makes my name red n stuff
 $('head').append('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/Potato-Helper/master/PH_CSS.css" type="text/css"/>');
 $('#chat-messages').append('<div style="width:300px;height:55px;border-left:3px solid cyan;"</div><i style="width:33px;height:30px;margin-left:5px;" class="icon icon-site-logo"></i><div style="color:cyan;height:25px;padding-left:45px;padding-top:7px;">Welcome to Potato Helper! If you are a potato this utility is for you! List of commands - /cmds</div>');
@@ -81,6 +81,7 @@ function PHchatCommands(data) { //the function to respond
 			alert('|PH| When you are no longer AFK please disable AFK mode by typing /back'); //Alerts user to turn off AFK mode
 			break;
 		case "/back":
+			$('#chat-input').css('background-color', '#282c35')
 			API.chatLog('Welcome back! AFK mode has been turned off.');
 			isAFK = false; //you are now no longer afk
 			break;

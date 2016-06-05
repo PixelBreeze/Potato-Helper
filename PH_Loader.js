e6af917f31943e76b11b03052aac7b0e0a225d40
PixelBreeze <thepixelbreeze@gmail.com>
2016-06-05 22:31:15 +0300

    Update PH_Loader.js

--- PH_Loader.js
+++ PH_Loader.js
@@ -5,22 +5,16 @@ $.getScript('https://code.radiant.dj/rs.min.js');
 function basic(data){
 this.data = data;
   if (data === '/reloadph') {
-      API.off(API.CHAT,Chater);
-      API.off(API.CHAT_COMMAND,chatCommands);
-       API.off(API.CHAT,Responses);
+
       $('#chat-messages').append('<div style="width:300px;height:30px;border-left:3px solid orange;"</div><i style="width:33px;height:30px;margin-left:5px;" class="icon icon-history-white"></i><div style="color:orange;height:25px;padding-left:45px;padding-top:7px;">|PH| Reloading...</div>');
       setTimeout(function(){$.getScript('https://rawgit.com/PixelBreeze/Potato-Helper/master/Main.js');},2000);
     }
   if (data ==='/phoff') {
-    API.off(API.CHAT,Chater);
-    API.off(API.CHAT_COMMAND,chatCommands);
-    API.off(API.CHAT,Responses);
+    
     API.chatLog("PH shutting down.")
   }
   if (data ==='/phon') {
-    API.on(API.CHAT,Chater);
-    API.on(API.CHAT_COMMAND,chatCommands);
-    API.on(API.CHAT,Responses);
+
     API.chatLog("PH Enabled");
   }
   }

d8d91c6cc36a542c8b909f78cb008624e79a2d3d
PixelBreeze <thepixelbreeze@gmail.com>
2017-05-26 21:38:38 +0300

    Update phv2.js

--- phv2.js
+++ phv2.js
@@ -1,16 +1,100 @@
-$('body').append('<style> [data-cid^="6175571"] .msg .from .un { color: rgba(250, 5, 54, 1) !important; } </style>'); //makes my name red n stuff
-$('head').append('<link rel="stylesheet" href="https://rawgit.com/PixelBreeze/Potato-Helper/master/PH_CSS.css" type="text/css"/>');
-$('#chat-messages').append('<div style="width:300px;height:55px;border-left:3px solid cyan;"</div><i style="width:33px;height:30px;margin-left:5px;" class="icon icon-site-logo"></i><div style="color:cyan;height:25px;padding-left:45px;padding-top:7px;">Welcome to Pixel Helper! List of commands - /cmds</div>');
-$('#chat-messages').append('<div style="width:300px;height:30px;border-left:3px solid cyan;"</div><div style="color:cyan;height:25px;padding-left:45px;padding-top:7px;">|PH| Credits: PixelBreezeNC</div>');
+//Fast Moderation
+$('head').append('<link id="pscss" rel="stylesheet" href="https://rawgit.com/PixelBreeze/PotatoScript/master/styles.css" type="text/css"/>');
+    $('body').append('<div id="psbutton" style="position: absolute; top: 77px; left: 1164px; width: 50px;"><div id="phbox"><img id="potatoimg" src="http://pngimg.com/uploads/potato/potato_PNG7078.png"/></div></div>');
+    $('#psbutton').append('<div id="pcbox" style="display: none;"><div id="pgot" class="pibox">Got</div><div id="phistory pimportant" class="pibox">History</div><div id="pskip" class="pibox pimportant">Skip</div><div id="pbltheme" class="pibox pimportant">BL Theme</div><div id="pbljunk" class="pibox pimportant">BL Junk</div><div id="pblop" class="pibox pimportant">BL OP</div><div id="preload" class="pibox">Reload</div></div>');
+    $("#confirmbox").toggle();
+//Draggable
+window.onload = addListeners();
+function addListeners(){
+    document.getElementById('phbox').addEventListener('mousedown', mouseDown, false);
+    window.addEventListener('mouseup', mouseUp, false);
+}
+function mouseUp()
+{
+    window.removeEventListener('mousemove', divMove, true);
+}
+function mouseDown(e){
+  window.addEventListener('mousemove', divMove, true);
+}
+function divMove(e){
+    var div = document.getElementById('psbutton');
+  div.style.position = 'absolute';
+  div.style.top = e.clientY + 'px';
+  div.style.left = e.clientX + 'px';
+}
+    
+    $("#confirmbox").toggle();
+    var pnext_move = 1;
+    var pconfirmed = false;
+    var action = " ";
+    $("#potatoimg").click(function() {
+        if (pnext_move == 1) {
+            $('#psbutton').animate({ 'width' : '273' });
+            pnext_move = 0;            
+        } else {
+            $('#psbutton').animate({ 'width' : '50' }); 
+            pnext_move = 1;
+        }
+    //   $('#tbutton').animate({width: '273px'});
+    });
+   
+   $("#potatoimg").click(function() {
+      $("#pcbox").toggle(); 
+   });
+    $("#pgot").click(function(){
+        API.sendChat('I got it.'); 
+    });
+    $("#phistory").click(function(){
+        action = "history";
+    });
+    $("#pskip").click(function(){
+        action = "skip";
+    });
+    $("#pbltheme").click(function(){
+        action = "theme";
+    });
+    $("#pbljunk").click(function(){
+        action = "junk"; 
+    });
+    $("#pblop").click(function(){
+        action = "op"; 
+    });
+    $("#preload").click(function(){
+        API.sendChat('/reload'); 
+    });
+   function executeaction() {
+         if (action === "skip") {
+            API.sendChat('!skip'); 
+         }
+         if (action === "theme") {
+            API.sendChat('!bl theme'); 
+         }
+         if (action === "junk") {
+            API.sendChat('!bl junk'); 
+         }
+         if (action === "history") {
+            API.sendChat('!skip'); 
+            API.sendChat('Song in History');
+         }
+      action = "";
+   }
+   $(".pibox.pimportant").click(function() {
+      var pconfirmed = confirm("Are You Sure?");
+      if (pconfirmed === true) {
+         executeaction();
+      }
+   });
 
 //                  -------- VARS --------- 
 var PH = function () {
-	version = "0.3";
+	version = "0.4";
 	this.version = version;
 	scriptName= "[PH] ";
 	delay = 2;
 	autoSkipVal = 0;
 	currentChannel = ""
+	UserCount = API.getUsers().length;
+	nMehs = 0;
 	thisPH = this;
 };
 
@@ -35,6 +119,10 @@ PH.prototype.onCmd = function(value){
 	}
 	if(value.toLowerCase().startsWith("/stream")){
 		thisPH.checkStream(value.substring(8));
+		return;
+	}
+	if(value.toLowerCase().startsWith("/mehrule")){
+		thisPH.mehRule();
 		return;
 	}
 	API.chatLog(scriptName + "Unknown command: " + value);
@@ -94,7 +182,7 @@ PH.prototype.autoSkip = function(val){
 	}
 };
 
-PH.prototype.checkStream = function(valu){
+/*PH.prototype.checkStream = function(valu){
 	currentChannel = parseInt(valu);
 		$.ajax({ 
 			 url:'https://api.twitch.tv/kraken/streams/' + valu,
@@ -114,14 +202,20 @@ PH.prototype.checkStream = function(valu){
 					 }
 				 }
 			});
-		}
+		} */ // WIP
+
+PH.prototype.mehRule = function(){
+	nMehs = Math.floor(UserCount - 100) / 10;
+	nMehs = Math.round(nMehs);
+	API.chatLog(nMehs.toString());
+};
 
 var phs = new PH();
 API.on(API.ADVANCE, phs.onAdvance);
 API.on(API.CHAT_COMMAND, phs.onCmd);
 
-/*function mehrulecalc() { //Calculates the needed mehs for a skip
+/* function mehrulecalc() { 
     var UserCount = API.getUsers().length; //user count in room
     var MehCalc = Math.floor(UserCount - 100) / 10;
     API.chatLog(MehCalc.toString());
-}
+} */
